22e0715fd98d25ec335d3639ae856787b7edfcf9
barlsym <barlsym@hotmail.com>
2019-05-12 21:31:54 +0300

    first commit

---
+++ src/main/java/org/hasandag/messenger/database/DatabaseClass.java
@@ -0,0 +1,26 @@
+package org.hasandag.messenger.database;
+
+import org.hasandag.messenger.model.Message;
+import org.hasandag.messenger.model.Profile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<String, Profile> profiles = new HashMap<>();
+
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<String, Profile> getProfiles() {
+		return profiles;
+	}
+
+	
+	
+	
+}
---
+++ src/main/java/org/hasandag/messenger/resources/MessageResource.java
@@ -0,0 +1,112 @@
+package org.hasandag.messenger.resources;
+
+import org.hasandag.messenger.model.Message;
+import org.hasandag.messenger.resources.beans.MessageFilterBean;
+import org.hasandag.messenger.service.MessageService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.List;
+
+
+@Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class MessageResource {
+
+	MessageService messageService = new MessageService();
+
+	@GET
+	public List<Message> getMessages(@BeanParam MessageFilterBean filterBean) {
+		if (filterBean.getYear() > 0) {
+			return messageService.getAllMessagesForYear(filterBean.getYear());
+		}
+		if (filterBean.getStart() >= 0 && filterBean.getSize() > 0) {
+			return messageService.getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
+		}
+		return messageService.getAllMessages();
+	}
+
+/*	@GET
+	@Path("/test")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String test() {
+		return "test called";
+	}*/
+
+//	@GET
+//	@Path("/{messageId}")
+//	public Message test(@PathParam("messageId") long id) {
+//		return messageService.getMessage(id);
+//	}
+
+	@PUT
+	@Path("/{messageId}")
+
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public Message deleteMessage(@PathParam("messageId") long id) {
+		return messageService.removeMessage(id);
+	}
+
+	@POST
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+	
+	@GET
+	@Path("/{messageId}")
+	public Message getMessage(@PathParam("messageId") long id, @Context UriInfo uriInfo) {
+		Message message = messageService.getMessage(id);
+		message.addLink(getUriForSelf(uriInfo, message), "self");
+		message.addLink(getUriForProfile(uriInfo, message), "profile");
+		message.addLink(getUriForComments(uriInfo, message), "comments");
+		
+		return message;
+		
+	}
+
+	private String getUriForComments(UriInfo uriInfo, Message message) {
+		URI uri = uriInfo.getBaseUriBuilder()
+				.path(MessageResource.class)
+	       		.path(MessageResource.class, "getCommentResource")
+	       		.path(CommentResource.class)
+	       		.resolveTemplate("messageId", message.getId())
+	            .build();
+	    return uri.toString();
+	}
+
+	private String getUriForProfile(UriInfo uriInfo, Message message) {
+		URI uri = uriInfo.getBaseUriBuilder()
+       		 .path(ProfileResource.class)
+       		 .path(message.getAuthor())
+             .build();
+        return uri.toString();
+	}
+
+	private String getUriForSelf(UriInfo uriInfo, Message message) {
+		String uri = uriInfo.getBaseUriBuilder()
+		 .path(MessageResource.class)
+		 .path(Long.toString(message.getId()))
+		 .build()
+		 .toString();
+		return uri;
+	}
+	
+	
+	
+	
+	@Path("/{messageId}/comments")
+	public CommentResource getCommentResource() {
+		return new CommentResource();
+	}
+
+}
