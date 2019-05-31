0d263eb7955e9a7ece68d7849d20c132bb31ccc7
barlsym <barlsym@hotmail.com>
2019-05-31 12:24:37 +0300

    First Message

---
+++ src/main/java/org.hasandag.demoservlet/DemoServlet2.java
@@ -0,0 +1,24 @@
+package org.hasandag.demoservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/demoPath2", "/DemoServlet2"}, name = "DemoServlet2")
+public class DemoServlet2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>DemoServlet2 Class doPost called'</h3>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>DemoServlet2 Class doGet called!</h3>");
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello World! (from JSP File)</h2>
 </body>
 </html>
